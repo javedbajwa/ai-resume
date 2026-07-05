d399b720d42cdc8d11d32f4a04e11bccb2145cf1
javedbajwa <javedbajwa2003@gmail.com>
2026-07-05 03:02:25 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+function App() {
+  return (
+    <>
+      <h1>AI Resume Screening System</h1>
+      <p>Welcome to my first AI project.</p>
+    </>
+  );
+}
+
+export default App;
